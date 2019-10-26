16c5dd8e57a0bbef48b3e119cd6a0452faa04457
hiaximize <dbarkerit@gmail.com>
2019-10-26 11:39:48 -0400

    changed server?

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const router = express.Router()
+
+app.use("/customer", router)
 
 app.get("/", (req, resp)=>{
     resp.json({
