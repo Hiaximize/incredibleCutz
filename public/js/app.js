e16af20f6fb28aed6ac1202748ea828e30fd8a8b
hiaximize <dbarkerit@gmail.com>
2019-11-05 15:08:44 -0500

    edited the main.css, app.js, server.js and index.ejs in views folder

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,1 @@
 console.log("connected")
-
-
-var header = document.getElementById("header")
-console.log(header)
-var sticky = header.offsetTop;
-
-function myFunction(){
-    if(window.pageYOffset > sticky){
-        header.classList.add("sticky")
-    } else {
-        header.classList.remove("sticky")
-    }
-}
-
-window.onscroll = function () { myFunction() };
