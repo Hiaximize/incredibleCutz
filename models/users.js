0ae33760b7413b3372ec5a6737c7abfd115dee4d
hiaximize <dbarkerit@gmail.com>
2019-11-17 15:13:01 -0500

    moved some info from barbers model to the users model. Going to get rid of barber model since they are one in the same technically

--- models/users.js
+++ models/users.js
@@ -5,7 +5,10 @@ const user = Schema({
     firstName: {type: String, require: true},
     lastName: {type: String, require: true},
     username: {type: String, require: true},
-    password: {type: String, require: true}
+    password: {type: String, require: true},
+    hometown: { type: String, require: true },
+    favCut: { type: String, require: true },
+    whatMadeYouCutHair: { type: String, require: true }
 })
 
 module.exports = mongoose.model('user', user)
--- views/app/controllers/customers.js
+++ views/app/controllers/customers.js
@@ -1,64 +1,64 @@
-const express = require('express')
-const customers = express.Router()
-const Customer = require('../models/customers.js')
+// const express = require('express')
+// const customers = express.Router()
+// const Customer = require('../models/customers.js')
 
-// INDEX ROUTE
-customers.get('/', (req, resp)=>{
-    Customer.find({}, (error, foundCustomers)=>{
-        if(error){
-            resp.send(error)
-        } else {
-            resp.json(foundCustomers)
-        }
-    })
-})
+// // INDEX ROUTE
+// customers.get('/', (req, resp)=>{
+//     Customer.find({}, (error, foundCustomers)=>{
+//         if(error){
+//             resp.send(error)
+//         } else {
+//             resp.json(foundCustomers)
+//         }
+//     })
+// })
 
-// SHOW ROUTE
-customers.get('/:id', (req, resp)=>{
-    Customer.findById(req.params.id, (error, foundUser)=>{
-        if(error){
-            resp.send(error)
-        } else {
-            resp.json(foundUser)
-        }
-    })
-})
+// // SHOW ROUTE
+// customers.get('/:id', (req, resp)=>{
+//     Customer.findById(req.params.id, (error, foundUser)=>{
+//         if(error){
+//             resp.send(error)
+//         } else {
+//             resp.json(foundUser)
+//         }
+//     })
+// })
 
-// UPDATE ROUTE
-customers.put('/:id', (req, resp)=>{
-    Customer.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedCustomer)=>{
-        if (error){
-            resp.send(error)
-        } else {
-            resp.json(updatedCustomer)
-        }
-    })
-})
+// // UPDATE ROUTE
+// customers.put('/:id', (req, resp)=>{
+//     Customer.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedCustomer)=>{
+//         if (error){
+//             resp.send(error)
+//         } else {
+//             resp.json(updatedCustomer)
+//         }
+//     })
+// })
 
-// DELETE ROUTE
-customers.delete('/:id', (req, resp)=>{
-    Customer.findByIdAndRemove(req.params.id, (error, deletedUser)=>{
-        if(error){
-            resp.send(error)
-        } else {
-            resp.json({
-                deleted: true,
-                deletedUser: deletedUser
-            })
-        }
-    })
-})
+// // DELETE ROUTE
+// customers.delete('/:id', (req, resp)=>{
+//     Customer.findByIdAndRemove(req.params.id, (error, deletedUser)=>{
+//         if(error){
+//             resp.send(error)
+//         } else {
+//             resp.json({
+//                 deleted: true,
+//                 deletedUser: deletedUser
+//             })
+//         }
+//     })
+// })
 
-// POST ROUTE
-customers.post('/', (req, resp)=>{
-    Customer.create(req.body, (error, createdUser)=>{
-        if(error){
-            resp.send(error)
-        } else {
-            resp.json(req.body)
-        // resp.redirect('/')
-        }
-    })
-})
+// // POST ROUTE
+// customers.post('/', (req, resp)=>{
+//     Customer.create(req.body, (error, createdUser)=>{
+//         if(error){
+//             resp.send(error)
+//         } else {
+//             resp.json(req.body)
+//         // resp.redirect('/')
+//         }
+//     })
+// })
 
-module.exports = customers
+// module.exports = customers
