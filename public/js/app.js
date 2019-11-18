51093fe0c6a1b90b2a330cce9d63ac51da5ea1c7
hiaximize <dbarkerit@gmail.com>
2019-11-18 08:47:10 -0500

    moved ajax call to the onclick function

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,11 @@
 console.log("connected")
 
-$.ajax({
-    method: "GET",
-    url: "http://incrediblecutz.herokuapp.com/customers/"
-}).then((data)=>{
-    console.log(data)
-})
 
 const getInfo=(_id)=>{
-   console.log(_id)
+    $.ajax({
+        method: "GET",
+        url: "http://incrediblecutz.herokuapp.com/customers/"+_id
+    }).then((data) => {
+        console.log(data)
+    })
 }
