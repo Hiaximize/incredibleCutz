95de07c1ecf78fe8bfb0bc094f1f365f4446c7d8
hiaximize <dbarkerit@gmail.com>
2019-11-18 07:20:06 -0500

    URL not working, copied and pasted in

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ console.log("connected")
 
 $.ajax({
     method: "GET",
-    url: "https://incrediblecutz.herokuapp.com/customers"
+    url: "http://incrediblecutz.herokuapp.com/customers/"
 }).then((data)=>{
     console.log(data)
 })
