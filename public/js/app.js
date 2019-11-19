ff5336e77e941a92173db636755d1bc4b0f602b2
hiaximize <dbarkerit@gmail.com>
2019-11-19 11:40:14 -0500

    added js file to the index page. Put ajax call for the users to grab the barber information to display on the front page

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,20 @@
 console.log("connected")
 
 
-const getInfo=(_id)=>{
-    $.ajax({
-        method: "GET",
-        url: "http://incrediblecutz.herokuapp.com/customers/" +_id
-    }).then((data) => {
-        console.log($(this).id)
-        console.log(data)
-    })
-}
+// const getInfo=(_id)=>{
+//     $.ajax({
+//         method: "GET",
+//         url: "http://incrediblecutz.herokuapp.com/customers/" +_id
+//     }).then((data) => {
+//         console.log($(this).id)
+//         console.log(data)
+//     })
+// }
+
+
+$.ajax({
+    method: "GET",
+    url: "http://incrediblecutz.herokuapp.com/users"
+}).then((data)=>{
+    console.log(data)
+})
