8bcc1a90cd0e38d94b71d88a41e04cfd40584ba4
hiaximize <dbarkerit@gmail.com>
2019-11-20 14:28:13 -0500

    added barber hours section and added the days of the week to the user model

--- models/users.js
+++ models/users.js
@@ -8,7 +8,14 @@ const user = Schema({
     password: {type: String, require: true},
     hometown: { type: String, require: true },
     favCut: { type: String, require: true },
-    whatMadeYouCutHair: { type: String, require: true }
+    whatMadeYouCutHair: { type: String, require: true },
+    monday: String,
+    tuesday: String,
+    wednesday: String,
+    thursday: String,
+    friday: String,
+    saturday: String,
+    sunday: String
 })
 
 module.exports = mongoose.model('user', user)
